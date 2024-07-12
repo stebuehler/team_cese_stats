1573177afd741de93bc91d2cdfe01a8f522e2d26
stebuehler <stibe.buehler@gmail.com>
2024-07-12 23:24:25 +0200

    added two files for heroku build

---
+++ init.R
@@ -0,0 +1,20 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+my_packages = c(
+  "shiny",
+  "DT",
+  "tidyverse",
+  "dplyr",
+  "googlesheets4",
+  "plotly",
+  "janitor",
+  "formattable"
+  )
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
